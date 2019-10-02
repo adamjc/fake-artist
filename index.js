a1210edf5f00028766153b9c6e0ece388f508913
adamjc <adamjc86@gmail.com>
2019-10-02 08:50:46 +0100

    We are going to use DynamoDB bois...

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-// connect to server
 const path = require('path')
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const DynamoDb = require('aws-sdk/clients/dynamodb')
+const db = new DynamoDb()
+
+console.log(db)
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(process.cwd(), './index.html'))
@@ -15,13 +18,16 @@ http.listen(3000, () => {
 
 io.on('connection', socket => {
   console.log(`connection received from ${socket.id}.`)
-  socket.emit('hello')
 
   socket.on('game:new', () => {
     console.log('creating a new game...')
     socket.emit('game:new', 'ABCD')
   })
-  
+
+  socket.on('game:start', () => {
+    console.log('starting a game')
+  })
+
   socket.on('game:join', id => {
     console.log(`connecting to room ${id}...`)
   })
