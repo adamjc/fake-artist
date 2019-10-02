efae80652a33f4ea91cc9b805bc733bba04d54a2
adamjc <adamjc86@gmail.com>
2019-10-02 20:21:53 +0100

    Add ddb adding / creating rooms

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const DynamoDb = require('aws-sdk/clients/dynamodb')
-const db = new DynamoDb()
+const AWS = require('aws-sdk')
+const hexgen = require('hex-generator')
 
-console.log(db)
+AWS.config.update({
+  region: 'eu-west-1',
+  endpoint: 'dynamodb.eu-west-1.amazonaws.com'
+})
+
+const db = new AWS.DynamoDB.DocumentClient()
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(process.cwd(), './index.html'))
@@ -21,14 +26,38 @@ io.on('connection', socket => {
 
   socket.on('game:new', () => {
     console.log('creating a new game...')
-    socket.emit('game:new', 'ABCD')
+
+    const roomId = hexgen(20).toString(16).toUpperCase()
+
+    const query = {
+      TableName: 'fake-artist',
+      Item: {
+        'roomId': roomId,
+        'users': {}
+      }
+    }
+
+    db.put(query, (err, data) => {
+      if (err) {
+        console.log(`err: ${JSON.stringify(err, null, 2)}`)
+      } else {
+        console.log(`data: ${JSON.stringify(data, null, 2)}`)
+        socket.join(roomId)
+        socket.emit('game:new', roomId)
+      }
+    })
   })
 
   socket.on('game:start', () => {
     console.log('starting a game')
+    
   })
 
   socket.on('game:join', id => {
     console.log(`connecting to room ${id}...`)
+  })
+
+  socket.on('disconnect', reason => {
+    console.log(`reason: ${JSON.stringify(reason, null, 2)}`)
   })
 })
