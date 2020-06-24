52c7d9beac2d39995ba34ec4bf6ec92cc5817798
adamjc <adamjc86@gmail.com>
2020-06-24 13:11:58 +0100

    add initial client-side webrtc

--- client/client.js
+++ client/client.js
@@ -28,9 +28,39 @@ function connect (roomId) {
 const msg = (messageType, message) => JSON.stringify({ messageType, message })
 
 function messageHandler ({ data }) {
-  console.log(data)
   const { messageType, ...message } = JSON.parse(data)
-  console.log(message)
+  
+  switch (messageType) {
+    case 'hosting':
+      // we are hosting the game
+      return 
+    case 'signal':
+      // make a webrtc connection to peer trying to answer/offer us
+      return
+    case 'joining':
+      // make a webrtc connection for every peer we receive in `message`
+      handleJoining(message.players)
+      return
+    case 'new-player':
+      // a new player has entered the room
+      return
+  }
+}
+
+// players = [{ 'connection_id', 'room_id' }]
+function handleJoining (players) {
+  const pcs = players.map(player => {
+    const peer = new SimplePeer({ initiator: true, trickle: false })
+
+    peer.on('signal', data => ws.send(msg('signal', data)))
+
+    peer.on('connect', console.log)
+    peer.on('data', console.log)
+    return {
+      connectionId: player.connection_id,
+      peer
+    }
+  })
 }
 
 document.getElementById('host').addEventListener('click', hostHandler)
