2526bb10ea2236961573702ade0d3cb0bb448468
adamjc <adamjc86@gmail.com>
2020-06-25 07:48:41 +0100

    Add client webrtc handling

--- client/client.js
+++ client/client.js
@@ -1,6 +1,9 @@
+let peers = []
+let ws
+
 function hostHandler () {
   console.log('hosting a game...')
-  connect()
+  ws = connect()
 }
 
 function joinHandler () {
@@ -8,12 +11,12 @@ function joinHandler () {
 
   console.log(`joining game ${roomId}`)
   
-  connect(roomId)
+  ws = connect(roomId)
 }
 
 function connect (roomId) {
   console.log('connect')
-  const ws = new WebSocket('wss://md04bdl83m.execute-api.eu-west-1.amazonaws.com/prod')
+  ws = new WebSocket('wss://md04bdl83m.execute-api.eu-west-1.amazonaws.com/prod')
   ws.onmessage = messageHandler
   ws.onopen = _ => {
     console.log('websocket opened')
@@ -23,23 +26,27 @@ function connect (roomId) {
       ws.send(msg('host'))
     }
   }
+
+  return ws
 }
 
 const msg = (messageType, message) => JSON.stringify({ messageType, message })
 
 function messageHandler ({ data }) {
   const { messageType, ...message } = JSON.parse(data)
+  console.log(messageType, message)
   
   switch (messageType) {
     case 'hosting':
       // we are hosting the game
       return 
     case 'signal':
+      handleSignal(message)
       // make a webrtc connection to peer trying to answer/offer us
       return
     case 'joining':
       // make a webrtc connection for every peer we receive in `message`
-      handleJoining(message.players)
+      peers = peers.concat(createPeers(message.players))
       return
     case 'new-player':
       // a new player has entered the room
@@ -47,8 +54,25 @@ function messageHandler ({ data }) {
   }
 }
 
+// connectionId: this is the connectionId of the remote peer we want to establish a webrtc connection with
+// data: this is the 'description' of the remote peer
+function handleSignal ({ connectionId, data }) {
+  let pc = peers.filter(peer => peer.connectionId === connectionId)
+
+  if (!pc) {
+    const peer = new SimplePeer({ initiator: false, trickle: false })
+    peer.signal(data)
+    peer.on('signal', data => ws.send(msg('signal', data)))
+    peer.on('connect', console.log)
+    peer.on('data', console.log)
+  } else {
+    pc.peer.signal(data)
+  }
+}
+
 // players = [{ 'connection_id', 'room_id' }]
-function handleJoining (players) {
+function createPeers (players) {
+  console.log('creating peers...')
   const pcs = players.map(player => {
     const peer = new SimplePeer({ initiator: true, trickle: false })
 
@@ -56,11 +80,14 @@ function handleJoining (players) {
 
     peer.on('connect', console.log)
     peer.on('data', console.log)
+
     return {
       connectionId: player.connection_id,
       peer
     }
   })
+
+  return pcs
 }
 
 document.getElementById('host').addEventListener('click', hostHandler)
