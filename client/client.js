927795358c405c77adb88b2066335d754bb6b632
adamjc <adamjc86@gmail.com>
2020-07-04 13:08:53 +0100

    Remove logging and minor refactor

--- client/client.js
+++ client/client.js
@@ -2,30 +2,20 @@ let peers = []
 let ws
 
 function hostHandler () {
-  console.log('hosting a game...')
   ws = connect()
 }
 
 function joinHandler () {
   const roomId = document.getElementById('roomId').value
-
-  console.log(`joining game ${roomId}`)
   
   ws = connect(roomId)
 }
 
 function connect (roomId) {
-  console.log('connect')
   ws = new WebSocket('wss://md04bdl83m.execute-api.eu-west-1.amazonaws.com/prod')
   ws.onmessage = messageHandler
-  ws.onopen = _ => {
-    console.log('websocket opened')
-    if (roomId) {
-      ws.send(msg('join', roomId))
-    } else {
-      ws.send(msg('host'))
-    }
-  }
+  ws.onopen = _ => roomId ? ws.send(msg('join', roomId)) : ws.send(msg('host'))
+  ws.onerror = e => console.log(`Web Socket error: ${JSON.stringify(e)}`)
 
   return ws
 }
@@ -33,63 +23,82 @@ function connect (roomId) {
 const msg = (messageType, message) => JSON.stringify({ messageType, message })
 
 function messageHandler ({ data }) {
+  console.log('messageHandler', data)
   const { messageType, ...message } = JSON.parse(data)
-  console.log(messageType, message)
   
   switch (messageType) {
     case 'hosting':
       // we are hosting the game
       return 
     case 'signal':
-      handleSignal(message)
       // make a webrtc connection to peer trying to answer/offer us
+      handleSignal(message)
       return
     case 'joining':
       // make a webrtc connection for every peer we receive in `message`
       peers = peers.concat(createPeers(message.players))
       return
     case 'new-player':
-      // a new player has entered the room
+      // a new player has entered the game
+      return
+    case 'disconnection':
+      // a player has left the game
+      peers = removePeer(message.connectionId)
       return
   }
 }
 
+const removePeer = connectionId => peers.filter(peer => peer.connectionId !== connectionId)
+
 // connectionId: this is the connectionId of the remote peer we want to establish a webrtc connection with
 // data: this is the 'description' of the remote peer
 function handleSignal ({ connectionId, data }) {
-  let pc = peers.filter(peer => peer.connectionId === connectionId)
-
+  let pc = peers.filter(peer => peer.connectionId === connectionId)[0]
   if (!pc) {
-    const peer = new SimplePeer({ initiator: false, trickle: false })
+    const peer = createPeer(connectionId)
+    
     peer.signal(data)
-    peer.on('signal', data => ws.send(msg('signal', { remoteId: peer.connectionId, data})))
-    peer.on('connect', console.log)
-    peer.on('data', console.log)
+    
+    peers = peers.concat({ connectionId, peer })
   } else {
+    console.log('found peer!', pc)
     pc.peer.signal(data)
   }
 }
 
 // players = [{ 'connection_id', 'room_id' }]
 function createPeers (players) {
-  console.log('creating peers...')
-  const pcs = players.map(player => {
-    const peer = new SimplePeer({ initiator: true, trickle: false })
+  return players.map(player => {
+    const peer = createPeer(player.connection_id, { initiator: true })
 
-    peer.on('signal', data => ws.send(msg('signal', data)))
-
-    peer.on('connect', console.log)
-    peer.on('data', console.log)
-
-    console.log(`signalling ${player.connection_id}`)
     return {
       connectionId: player.connection_id,
       peer
     }
   })
+}
 
-  return pcs
+function createPeer (remoteId, initiator = false) {
+  const peer = new SimplePeer({ initiator })
+
+  peer.on('signal', data => ws.send(msg('signal', { remoteId, data })))
+  peer.on('connect', _ => console.log('peer connect'))
+
+  peer.on('data', e => {
+    const message = document.createElement('div')
+    message.innerHTML = e
+    document.getElementById('message__response').appendChild(message)
+  })
+
+  return peer
 }
 
 document.getElementById('host').addEventListener('click', hostHandler)
 document.getElementById('join').addEventListener('click', joinHandler)
+document.getElementById('message__button').addEventListener('click', sendMessage)
+
+function sendMessage() {
+  console.log('sending message yo')
+  const msg = document.getElementById('message').value
+  peers.forEach(peer => peer.peer.send(msg))
+}
